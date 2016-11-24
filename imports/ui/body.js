dbd6b6bebec55847d0bf6dcda0cea272f4e4044d
Juan Marroquin <juam95@stanford.edu>
2016-11-24 00:32:06 -0800

    1. Added status message about saving 2. Added message indicating if a post was cached 3. Added conditional retrieval of uncached content

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -40,25 +40,6 @@ Template.Page_Template.helpers({
 });
 
 Template.Page_Template.events({
-  // 'submit .new-issue'(event) {
-  //   event.preventDefault();
-  //   subjectObject = $(".new-issue [name='Subject']");
-  //   detailsObject = $(".new-issue [name='Details']");
-  //   const subjectText = subjectObject.val();
-  //   const detailsText = detailsObject.val();
-  //   console.log(subjectText)
-  //   console.log(detailsText)
-  //   Issues.insert({
-  //     subject: subjectText,
-  //     details: detailsText,
-  //     steps: [],
-  //     createdAt: new Date(),
-  //     owner: Meteor.userId(),
-  //     username: Meteor.user().username,
-  //   });
-  //   detailsObject.val("");
-  //   subjectObject.val("");
-  // },
   'change .show-open input'(event, instance) {
     instance.state.set('showOpen', event.target.checked)
   }
