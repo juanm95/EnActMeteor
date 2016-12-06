fc3d33460ece0feae59b6365f6177f40bd3c1e88
Juan Marroquin <juam95@stanford.edu>
2016-12-05 22:43:49 -0800

    Adding social config

---
+++ server/social-config.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+ 
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '1293600994030280',
+    secret: '806ccbcdcd0ec018e1a82f2d11f5755c'
+});
